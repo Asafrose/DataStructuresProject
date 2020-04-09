3ca8ba96dddf1d0af309aacd0bb1f34c78cc30bf
Asafrose <asafr1993@gmail.com>
2020-04-09 16:49:59 +0300

    Infra | Add Comments

--- src/DataStructuresProject/Program.cpp
+++ src/DataStructuresProject/Program.cpp
@@ -4,3 +4,17 @@ int main()
 {
     std::cout << "Hello World!\n";
 }
+
+/*
+ *  AlloctingList
+ *      AllocatingListNode
+ *
+ *  StaticList
+ *      StaticListNode
+ *
+ *  Stack
+ *      AllocatingList
+ *
+ *  ItemType
+ *      
+ */
