ea32ff070529ee2ecaf03b6bc4f75172f37880fe
Asafrose <asafr1993@gmail.com>
2020-04-10 12:18:29 +0300

    Feature | Add NetworkInputHelper logic

--- src/DataStructuresProject/NetworkInputHelper.cpp
+++ src/DataStructuresProject/NetworkInputHelper.cpp
@@ -1,1 +1,25 @@
 ﻿#include "NetworkInputHelper.h"
+
+#include <iostream>
+
+using namespace std;
+
+AllocatingList<int>* NetworkInputHelper::GetNetwork(int& size)
+{
+	cin >> size;
+	size++;
+	AllocatingList<int>* result = new AllocatingList<int>[size];
+
+	int connectionsCount;
+	cin >> connectionsCount;
+	for (int i = 0; i < connectionsCount; ++i)
+	{
+		int sourceComputer;
+		int destinationComputer;
+
+		cin >> sourceComputer >> destinationComputer;
+		result[sourceComputer].Add(destinationComputer);
+	}
+
+	return result;
+}
